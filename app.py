981626e36b04911230b529df69fc9cec0c2bc938
Fredxx0407 <93413794+Fredxx0407@users.noreply.github.com>
2021-11-06 11:37:08 +0800

    Update app.py

--- app.py
+++ app.py
@@ -145,6 +145,9 @@ def aqi_chart_24h():
         aqi_list_time.append( item['time'] )
 
     plt.plot(aqi_list_time,aqi_list,'b-o')
+    plt.xlabel('Time', fontproperties = font)
+    plt.ylabel('api', fontproperties = font)
+    plt.xticks(aqi_list_time, rotation=90)
     plt.grid()
     plt.savefig('img.png')
     plt.close()
